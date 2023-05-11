4a6923dff39eeacfd054c4d2d73e7e75e0d538a3
KPflitsch <Kian.Pflitsch@city.ac.uk>
2023-05-11 14:21:36 +0100

    Added comments and finished insert function

--- main.py
+++ main.py
@@ -23,38 +23,83 @@ filename =r"C:\Users\KianP\Desktop\Kian NEA\Database.db"
 
 def createTableManInfo():
     try:
+        ##with statement means will only run if script is accessable
         with open('createmanagerinfo.sql', "r") as sql_command:
+            ##reading the sql file
             sql = sql_command.read()
             
-            cnn = sqlite3.connect(filename)
+            ##connecting the database so that changes can be made
+            conn = sqlite3.connect(filename)
             print("Database connected")
             
-            cs = cnn.cursor()
-            cs.executescript(sql)
-            cnn.commit()
+            ##cursor is needed to create/modify/insert in a database
+            cursor = cnn.cursor()
+            ##running sql file
+            cursor.executescript(sql)
+            ##commiting changes to the database
+            conn.commit()
             
-            sql = 'SELECT * FROM managerInfo;'
+            ##fetching all added records so that they can be printed for debug
+            records = cursor.fetchall()
             
-            cs.execute(sql)
-            records = cs.fetchall()
-            
+            ##printing records for debug
             for rec in records:
                 print(rec)
 
+    ##throws an error in case database file is not found
+    ##or if SQL script you want to fun is not found or has bad syntax
+    except Error as e:
+        print(e)
+    
+    ##Closes the database again after use because it is good practice
+    finally:
+        if conn:
+            conn.close()
+            
+            
+def insertDataManInfo():
+    try:
+        ##connecting the database and creating a cursor
+        conn = sqlite3.connect(filename)
+        cursor = conn.cursor()
+        
+        ##simple bool for the use of confirming email
+        emailGood = False
+        
+        ##asking for inputs of data
+        name = input("Enter full name: ")
+        
+        ##while loop so user can confirm email
+        while not emailGood:
+            emailOne = input("Enter email address: ")
+            emailTwo = input("Confirm email address: ")
+            if emailOne == emailTwo:
+                emailGood = True
+            else:
+                print("Email adressed do not match")
+        
+        hourRate = int(input("Input your hourly rate: "))
+        
+        ##Inserting the inputs into the database
+        cursor.execute("INSTERT INTO managerInfo(member_ID, name, email, hourRate) VALUES (?,?,?,?)")
+        ##Comitting the changes
+        conn.commit()
+        print("New data inserted")
+        
+    
     except Error as e:
         print(e)
     
     finally:
-        if cnn:
-            cnn.close()
-            
-def insertDataManInfo():
+        if conn:
+            conn.close()
     
             
 if __name__ == '__main__':
     ##CHANGE THE FILEPATH TO WHERE YOU WANT YOUR DATABASE TO BE CREATED AND WHAT TO NAME IT
     create_connection(r"C:\Users\KianP\Desktop\Kian NEA\Database.db")
     createTableManInfo()
+    insertDataManInfo()
     
             
 
